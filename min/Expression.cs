abe6f440f14285b6fc390a2b89b463de83567f34
PandawanFr <pandawanfr@gmail.com>
2019-05-21 21:54:48 -0700

    Got Expression working

--- min/Expression.cs
+++ min/Expression.cs
@@ -1,46 +1,76 @@
-namespace min
+namespace min {
+public abstract class Expression
 {
-    public interface IExpression { }
+public abstract T Accept<T>(IVisitor<T> visitor);
 
-    public struct BinaryExpression : IExpression
-    {
-        public readonly IExpression left;
-        public readonly Token @operator;
-        public readonly IExpression right;
-        public BinaryExpression(IExpression left, Token @operator, IExpression right)
-        {
-            this.left = left;
-            this.@operator = @operator;
-            this.right = right;
-        }
-    }
+public interface IVisitor<T>
+{
+T VisitBinary (Binary expression);
+T VisitGrouping (Grouping expression);
+T VisitLiteral (Literal expression);
+T VisitUnary (Unary expression);
+}
 
-    public struct GroupingExpression : IExpression
-    {
-        public readonly IExpression expression;
-        public GroupingExpression(IExpression expression)
-        {
-            this.expression = expression;
-        }
-    }
+public class Binary : Expression
+{
+public readonly Expression left;
+public readonly Token @operator;
+public readonly Expression right;
+public Binary (Expression left, Token @operator, Expression right)
+{
+this.left = left;
+this.@operator = @operator;
+this.right = right;
+}
 
-    public struct LiteralExpression : IExpression
-    {
-        public readonly object value;
-        public LiteralExpression(object value)
-        {
-            this.value = value;
-        }
-    }
+public override T Accept<T>(IVisitor<T> visitor)
+{
+return visitor.VisitBinary(this);
+}
+}
 
-    public struct UnaryExpression : IExpression
-    {
-        public readonly Token @operator;
-        public readonly IExpression right;
-        public UnaryExpression(Token @operator, IExpression right)
-        {
-            this.@operator = @operator;
-            this.right = right;
-        }
-    }
+public class Grouping : Expression
+{
+public readonly Expression expression;
+public Grouping (Expression expression)
+{
+this.expression = expression;
+}
+
+public override T Accept<T>(IVisitor<T> visitor)
+{
+return visitor.VisitGrouping(this);
+}
+}
+
+public class Literal : Expression
+{
+public readonly object value;
+public Literal (object value)
+{
+this.value = value;
+}
+
+public override T Accept<T>(IVisitor<T> visitor)
+{
+return visitor.VisitLiteral(this);
+}
+}
+
+public class Unary : Expression
+{
+public readonly Token @operator;
+public readonly Expression right;
+public Unary (Token @operator, Expression right)
+{
+this.@operator = @operator;
+this.right = right;
+}
+
+public override T Accept<T>(IVisitor<T> visitor)
+{
+return visitor.VisitUnary(this);
+}
+}
+}
 }
--- tools/GenerateAst.cs
+++ tools/GenerateAst.cs
@@ -19,23 +19,29 @@ namespace tools
 
             string outputDir = args[0];
 
-
-
             DefineAst(outputDir, "Expression", new List<string>() {
-                "BinaryExpression   : IExpression left, Token @operator, IExpression right",
-                "GroupingExpression : IExpression expression",
-                "LiteralExpression  : object value",
-                "UnaryExpression    : Token @operator, IExpression right"
+                "Binary   : Expression left, Token @operator, Expression right",
+                "Grouping : Expression expression",
+                "Literal  : object value",
+                "Unary    : Token @operator, Expression right"
             });
         }
 
+        // Generate Expression file with namespace, interface class, and all Expressions
         private static void DefineAst(string outputDir, string baseName, List<string> types)
         {
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(outputDir, $"{baseName}.cs")))
             {
                 outputFile.WriteLine("namespace min {");
 
-                outputFile.WriteLine("public interface IExpression {}");
+                outputFile.WriteLine("public abstract class Expression");
+                outputFile.WriteLine("{");
+
+                // Add a base accept method
+                outputFile.WriteLine("public abstract T Accept<T>(IVisitor<T> visitor);");
+                outputFile.WriteLine();
+
+                DefineVisitor(outputFile, baseName, types);
 
                 foreach (string type in types)
                 {
@@ -45,22 +51,29 @@ namespace tools
                 }
 
                 outputFile.WriteLine("}");
+
+                outputFile.WriteLine("}");
             }
         }
 
+        // Generate one Expression struct + fields + ctor
         private static void DefineType(StreamWriter outputFile, string baseName, string className, string fields)
         {
             outputFile.WriteLine();
-            outputFile.WriteLine($"public struct {className} : IExpression");
+            outputFile.WriteLine($"public class {className} : Expression");
             outputFile.WriteLine("{");
 
             string[] fieldList = fields.Split(", ");
 
+            // Fields
             foreach (string field in fieldList)
             {
                 outputFile.WriteLine($"public readonly {field};");
             }
 
+            Console.WriteLine();
+
+            // Constructor
             outputFile.WriteLine($"public {className} ({fields})");
             outputFile.WriteLine("{");
 
@@ -71,6 +84,28 @@ namespace tools
             }
 
             outputFile.WriteLine("}");
+
+            // Visitor pattern
+            outputFile.WriteLine();
+            outputFile.WriteLine("public override T Accept<T>(IVisitor<T> visitor)");
+            outputFile.WriteLine("{");
+            outputFile.WriteLine($"return visitor.Visit{className}(this);");
+            outputFile.WriteLine("}");
+
+            outputFile.WriteLine("}");
+        }
+
+        // Generate visitor interface to use Expressions
+        private static void DefineVisitor(StreamWriter outputFile, string baseName, List<string> types)
+        {
+            outputFile.WriteLine("public interface IVisitor<T>");
+            outputFile.WriteLine("{");
+
+            foreach (string type in types)
+            {
+                string typeName = type.Split(":")[0].Trim();
+                outputFile.WriteLine($"T Visit{typeName} ({typeName} {baseName.ToLower()});");
+            }
 
             outputFile.WriteLine("}");
         }
